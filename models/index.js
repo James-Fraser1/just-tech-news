29d045a3d63c5a7a07b8a79c436b49a7eefd5933
James-Fraser1 <jdfraser2022@gmail.com>
2022-08-29 11:23:28 -0400

    module 13 async changes all complete

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -29,15 +30,31 @@ Vote.belongsTo(User, {
 });
 
 Vote.belongsTo(Post, {
-  foreignKey: "post_id"
+  foreignKey: 'post_id'
 });
 
 User.hasMany(Vote, {
-  foreignKey: "user_id"
+  foreignKey: 'user_id'
 });
 
 Post.hasMany(Vote, {
-  foreignKey: "post_id"
+  foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
